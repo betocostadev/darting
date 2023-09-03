458ffdb219968cf1c329c0c2d7a643b6bf57639f
betocostadev <robertomarx@gmail.com>
2023-09-03 14:38:21 -0300

    added some test cases

--- dart-testing/lib/dart_testing.dart
+++ dart-testing/lib/dart_testing.dart
@@ -1,3 +1,12 @@
-int calculate() {
-  return 6 * 7;
+double getDiscount(double value, double discount, bool isPercent) {
+  if (value <= 0) {
+    throw ArgumentError('Product price cannot be 0 or lower');
+  }
+  if (discount <= 0) {
+    throw ArgumentError('Discount must be above 0');
+  }
+  if (isPercent) {
+    return value - value * (discount / 100);
+  }
+  return value - discount;
 }
--- dart-testing/test/dart_testing_test.dart
+++ dart-testing/test/dart_testing_test.dart
@@ -2,7 +2,58 @@ import 'package:dart_testing/dart_testing.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
+  test('get discount using percentage', () {
+    expect(getDiscount(500, 10, true), equals(450));
+  });
+
+  test('get discount using percentage and zero discount', () {
+    expect(
+        () => getDiscount(500, 0, true), throwsA(TypeMatcher<ArgumentError>()));
+  });
+
+  test('get discount not using percentage, and using product price as zero',
+      () {
+    expect(
+        () => getDiscount(0, 10, false), throwsA(TypeMatcher<ArgumentError>()));
+  });
+
+  test('get discount not using percentage', () {
+    expect(getDiscount(700, 50, false), equals(650));
+  });
+
+  group('calculate product prices with discount: ', () {
+    var valuesToTest = {
+      {'value': 1500, 'discount': 300, 'isPercent': false}: 1200,
+      {'value': 1500, 'discount': 20, 'isPercent': true}: 1200
+    };
+
+    valuesToTest.forEach((values, expected) {
+      test('$values: $expected', () {
+        expect(
+            getDiscount(
+                double.parse(values['value'].toString()),
+                double.parse(values['discount'].toString()),
+                values['isPercent'] == true),
+            equals(expected));
+      });
+    });
+  });
+
+  group('calculate product using invalid parameters: ', () {
+    var valuesToTest = {
+      {'value': 500, 'discount': 0, 'isPercent': true},
+      {'value': 0, 'discount': 10, 'isPercent': false}
+    };
+
+    for (var values in valuesToTest) {
+      test('$values: Error', () {
+        expect(
+            () => getDiscount(
+                double.parse(values['value'].toString()),
+                double.parse(values['discount'].toString()),
+                values['isPercent'] == true),
+            throwsA(TypeMatcher<ArgumentError>()));
+      });
+    }
   });
 }
